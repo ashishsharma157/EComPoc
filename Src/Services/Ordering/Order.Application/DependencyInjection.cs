af7a0898b1eedfe91eb36055c96cca93674b8329
ashishsharma157 <sharma.ashish78@gmail.com>
2025-02-16 19:20:57 -0800

    Entityframework started

--- Src/Services/Ordering/Order.Application/DependencyInjection.cs
+++ Src/Services/Ordering/Order.Application/DependencyInjection.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Order.Application
+namespace Ordering.Application
 {
     public static class DependencyInjection
     {
